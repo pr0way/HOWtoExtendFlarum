158df12212f125a325325ab17555eb4e6ac05795
Tpk <admin@tcgforum.pl>
2018-01-23 23:12:37 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ namespace anon\HelloWorld;
 //use Flarum\Foundation\Application;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) { //literowka dispatcher
+return function (Dispatcher $events) {
     // echo 'Hello, world!';
 
     $events->subscribe(Listeners\AddAssets::class);
